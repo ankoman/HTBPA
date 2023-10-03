ac8e8e22dbc634aa34edf9963f143dddc6675f12
Junichi Sakamoto <i08320.wctsonhb@gmail.com>
2023-10-03 23:15:15 +0900

    Change TOP module. Use '-mode out_of_context' synthesis option.

--- src/TOP_pairing.sv
+++ src/TOP_pairing.sv
@@ -20,74 +20,49 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-localparam USE_BRAM_IP = 1;
+localparam N_CORES = 8;
 
 module TOP_pairing(
     input   default_sysclk1_300_clk_n,
             default_sysclk1_300_clk_p,
             CPU_RESET,  //Active high
-            USB_UART_TX,
-input [7:0] addr,
-    output  USB_UART_RX
+            RUN,
+            LATCH,
+    input [12:0] addr,
+    output redundant_poly_L3 extout
     );
+    
+    wire clk;
+    redundant_poly_L3[N_CORES-1:0] buf_out;
+    redundant_poly_L3[N_CORES-1:0] pairing_out;
+    assign extout = buf_out[addr[12:8]];
+    
+    for(genvar i = 0; i < N_CORES; i = i + 1) begin
+        BN254_pairing DUT
+            (.clk,
+             .rstn(!CPU_RESET), 
+             .run(RUN), 
+             .n_func(4'd0), 
+             .extin_addr(), 
+             .extout_addr(addr[7:0]), 
+             .extin_data(), 
+             .extin_en(), 
+             .extout_data(pairing_out[i])
+             );
+             
+         always @(posedge clk) begin
+            if(CPU_RESET)
+                buf_out[i] <= '0;
+            else if (LATCH)
+                buf_out[i] <= pairing_out[i];
 
-    wire [288:0]Net, Net1, Net2;
-    wire [288:0]blk_mem_gen_0_doutb;
-    wire [288:0]blk_mem_gen_1_doutb;
-    wire clk_wiz_0_clk_out1, clk_100M;
-    wire [0:0]xlconstant_0_dout = '1;
-    wire [8:0]xlconstant_1_dout = '0;
-    wire [8:0]xlconstant_2_dout = '1;
-
-
-    Pairing_UART u0(
-            .clk_uart(clk_100M),
-            .clk(clk_wiz_0_clk_out1),
-            .addr(addr),
-            .rstn(!CPU_RESET),
-            .uart_tx(USB_UART_TX),
-            .uart_rx(USB_UART_RX)
-    );
-
-
-    // assign USB_UART_RX = Net[271];
-    // //extendedEuclidean5 dut (.a(blk_mem_gen_1_doutb), .p(PARAMS_BN254_d0::Mod), .a_inv (Net), .startflag(blk_mem_gen_0_doutb[0]), .clk(clk_wiz_0_clk_out1), .rstn(CPU_RESET));
-    // Mont_inv_multi DUT (.clk(clk_wiz_0_clk_out1), .rstn(CPU_RESET), .I_START(blk_mem_gen_0_doutb[0]), .I_DATA_N(257'(PARAMS_BN254_d0::Mod)), .I_WDATA(blk_mem_gen_1_doutb), .O_RDATA(Net));
+         end
+    end
 
     clk_wiz_600 clk_wiz
         (.clk_in1_n(default_sysclk1_300_clk_n),
         .clk_in1_p(default_sysclk1_300_clk_p),
-        .clk_out1(clk_wiz_0_clk_out1),
-        .clk_out2(clk_100M));
-
-endmodule
-
-
-module Pairing_UART(
-    input   clk_uart,
-            clk,
-            rstn,
-            uart_tx,
-input [7:0] addr,
-    output uart_rx
-    );
-    
-        wire [304-1:0] extout_data;
-        logic [304-1:0] reg_extout_data;
-        assign uart_rx = reg_extout_data[200];
-
-
-    BN254_pairing DUT(.clk, .rstn, .run(uart_tx), .n_func(4'd0), .extin_addr(), .extout_addr(addr), .extin_data(), .extin_en(), .extout_data);
-
-    always @(posedge clk) begin
-        if(!rstn)
-                reg_extout_data <= '0;
-        else begin
-                if (addr[7])
-                        reg_extout_data <= extout_data;
-                else
-                        reg_extout_data <= {reg_extout_data[302:0], 1'b0};
-        end
-    end
+        .clk_out1(clk)
+        );
 
 endmodule
